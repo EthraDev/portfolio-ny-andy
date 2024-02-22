c0749ff409505df6dd47c4bce6bc0bc3f28e64ae
Thea Blachon <thea.blachon73@gmail.com>
2024-02-22 16:17:12 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Title from './components/Title.vue';
+</script>
+
+<template>
+  <main>
+
+    <div>
+      <Title title="DEVELOPPEUR BACKEND" class="margin-top"/>
+      <Title title="PRODUCTEUR AUDIOVISUEL" />
+      <img src="./assets/nyandylpb.png" alt="ny_andy" class="img-ny-andy">
+    </div>
+
+    <Title title="NY ANDY" class="margin-top position-right"/>
+    <Title title="RADRIANARIMANANA" class="position-right"/>
+  </main>
+</template>
+
+<style scoped>
+.margin-top {
+    margin-top: 50px;
+}
+
+.img-ny-andy {
+    width: 555px;
+    height: 250px;
+    object-fit: cover;
+    object-position: 50% 30%;
+}
+
+.position-right {
+    text-align: right;
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2 :class="titleClass">{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        titleClass: 'title-style',
+    };
+  },
+  props: {
+    title: {
+        type: String,
+        required: true,
+    }
+  }
+};
+</script>
+
+<style scoped>
+h2 {
+    color: white;
+    font-family: "Montserrat", sans-serif;
+    font-size: 3rem;
+    letter-spacing: 5px;
+}
+</style>
